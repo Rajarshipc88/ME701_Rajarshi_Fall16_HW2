0aa880c8baa1c72e49ca76d56e6c3435cb31fa20
Rajarshipc88 <rajarshipc@ksu.edu>
2016-09-27 15:42:26 -0500

    Delete bash3.sh

--- bash3.sh
+++
@@ -1,7 +0,0 @@
-# This bash file moves a file to a certain directory names trash and/or creates it if it does not exist.
-
- #cd /me701ex/ME701_Rajarshi_Fall16_HW2
-gedit file1
-mv  -f file1 trash 
-
-#cp -R file1 trash
